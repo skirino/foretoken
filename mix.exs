457952780bbc58f510364da930656f6c19dfbce9
Shunsuke Kirino <skirino@flywheel.jp>
2023-04-08 22:45:48 +0900

    Update dev/test-only deps

--- mix.exs
+++ mix.exs
@@ -29,9 +29,9 @@ defmodule Foretoken.MixProject do
   defp deps() do
     [
       {:croma      , "~> 0.9"},
-      {:dialyxir   , "~> 0.5"   , [only: :dev ]},
-      {:ex_doc     , "~> 0.18.0", [only: :dev ]},
-      {:excoveralls, "~> 0.10"  , [only: :test]},
+      {:dialyxir   , "~> 1.3" , [only: :dev , runtime: false]},
+      {:ex_doc     , "~> 0.29", [only: :dev , runtime: false]},
+      {:excoveralls, "~> 0.16", [only: :test, runtime: false]},
     ]
   end
 
